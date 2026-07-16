da3868de6b68f92b3a6a029f49ce20bcf444c559
code-908 <ospanovjamshid65@gmail.com>
2026-07-16 16:48:08 +0500

    df

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,10 @@
 import os
 
+bind = f"0.0.0.0:{os.environ.get('PORT', 8000)}"
 bind = "0.0.0.0:8000"
 workers = int(os.environ.get("WEB_CONCURRENCY", 3))
 timeout = 120
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
+bind = f"0.0.0.0:{os.environ.get('PORT', 8000)}"
