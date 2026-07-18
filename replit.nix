cc463b3e45ba728daa8503fc48e948b3e97bdc0e
code-908 <ospanovjamshid65@gmail.com>
2026-07-18 14:32:25 +0500

    jamshid

--- replit.nix
+++ replit.nix
@@ -3,12 +3,16 @@
     pkgs.python311
     pkgs.python311Packages.pip
     pkgs.python311Packages.setuptools
+    pkgs.python311Packages.virtualenv
     pkgs.pkg-config
     pkgs.glibcLocales
+    pkgs.glib
+    pkgs.stdenv.cc.cc
   ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
       pkgs.stdenv.cc.cc.lib
+      pkgs.glib
     ];
   };
 }
